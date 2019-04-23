6c639202c1da27b3112887224df4b5efbc9d8c15
Ryan Anderson <ryan.anderson@buzzuti.com>
2019-04-23 17:16:14 -0500

    updated all packages.

--- src/Web/Scripts/_references.js
+++ src/Web/Scripts/_references.js
@@ -1,4 +1,4 @@
-﻿/// <reference path="jquery-2.1.3.js" />
+﻿/// <reference path="jquery-3.3.1.js" />
 /// <autosync enabled="true" />
 /// <reference path="bootstrap.js" />
 /// <reference path="jquery.validate.js" />
